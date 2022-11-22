570e8bfb4997ec515339b300292bceeeef00288b
Atul Choudhary <atulchoudhary.2908@gmail.com>
2022-11-23 00:08:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+
+    // Implement all routes here
+    <Router>
+      <Routes>
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+    
+    
+
+  );
+}
+
+export default App;
+
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://realtime-chat-application-fpv.vercel.app/';
+
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+
+    // Fetch data from url query string and console.log it
+
+    console.log(queryString.parse(window.location.search));
+    socket = io(ENDPOINT, { transports: ['websocket', 'polling', 'flashsocket'] });
+
+    setRoom(room);
+    setName(name)
+
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, []);
+  
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
+    });
+    
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+}, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+      <TextContainer users={users}/>
+    </div>
+  );
+}
+
+export default Chat;
